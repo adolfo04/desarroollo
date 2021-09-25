21686a4a823d3be2292fa11a8601134ced417f50
adolfo04 <56764770+adolfo04@users.noreply.github.com>
2021-09-25 13:04:12 -0600

    Update pag.php

--- form/formulario/pag.php
+++ form/formulario/pag.php
@@ -8,7 +8,7 @@
 <body style="background-color:#00fff2;">
 </body>
   <?php
-    echo "<b>El CUI ingresado es </b>:" . $_REQUEST['cui'];
+    echo "<b>El nit ingresado es </b>:" . $_REQUEST['cui'];
     echo "<br>";
 
     echo "<b>Primer Nombre: </b>" . $_REQUEST['primernombre'];
@@ -32,27 +32,11 @@
     echo "<b>El telefono es: </b>" . $_REQUEST['telefono'];
     echo "<br>";
 
-    echo "<b>La universidad es: </b>" . $_REQUEST['select'];
+    echo "<b>La tienda es: </b>" . $_REQUEST['select'];
     echo "<br>";
 
-    echo "<b>Tu Promedio: </b>" . $_REQUEST['promedio'];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
     
-    $precio = 900;
-    $promedio = $_REQUEST['promedio'];
     
-    if ($promedio > 89)
-    {
-      echo "<b>Por tener el promedio Mayor a 89 tienes un descuento del 70% y pagas Q.</b>" . ($precio*0.7);
-    }
-    elseif ($promedio >=80 && $promedio <=89){
-      echo "<b>Por tener el promedio entre 80 y 89 tienes un descuento del 40% y pagas Q.</b>" . ($precio*0.4);
-    }
-    elseif ($promedio < 80){
-      echo "<b>Por tener el Promedio menor a 80 no tienes ningun descuento y pagas Q.</b>" . ($precio);
-    }
     
  
   ?>
